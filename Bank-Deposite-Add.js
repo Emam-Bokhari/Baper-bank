867be72dd026f5f17a72ac85dd61caad897cac72
Moshfiqur Rahman <moshfiqurrahman37@gmail.com>
2023-08-16 09:48:07 +0600

    Bank website done

--- Bank-Deposite-Add.js
+++ Bank-Deposite-Add.js
@@ -1,18 +1,39 @@
-document.querySelector('#deposit-btn').addEventListener('click', function(){
-    const inputDeposit=document.querySelector('#input-deposit');
-    const inputDepositValue=inputDeposit.value;
-    const convertInputDepositValue=parseFloat(inputDepositValue);
+// Deposit
+document.querySelector('#deposit-btn').addEventListener('click', function () {
+    const inputDeposit = document.querySelector('#input-deposit');
+    const inputDepositValue = inputDeposit.value;
+    const convertInputDepositValue = parseFloat(inputDepositValue);
     // step 02
-    const previousDeposit=document.querySelector('#previous-deposit');
-    const previousDepositValue=previousDeposit.innerText;
-    const previousDepositValueConvert=parseFloat(previousDepositValue);
-   const currentTotalDeposit=previousDepositValueConvert+convertInputDepositValue;
-   previousDeposit.innerText=currentTotalDeposit;
-    // step 03
-   const balance= document.querySelector('#balance');
-   const balanceValue=balance.innerText;
-   const balanceValueConvert=parseFloat(balanceValue);
-   const currentBalanceTotal=balanceValueConvert+convertInputDepositValue;
-   balance.innerText=currentBalanceTotal;
-   document.querySelector('#input-deposit').value='';
+    const previousDeposit = document.querySelector('#previous-deposit');
+    const previousDepositValue = previousDeposit.innerText;
+    const previousDepositValueConvert = parseFloat(previousDepositValue);
+    const currentTotalDeposit = previousDepositValueConvert + convertInputDepositValue;
+    previousDeposit.innerText = currentTotalDeposit;
+    // balance
+    const balance = document.querySelector('#balance');
+    const balanceValue = balance.innerText;
+    const balanceValueConvert = parseFloat(balanceValue);
+    const currentBalanceTotal = balanceValueConvert + convertInputDepositValue;
+    balance.innerText = currentBalanceTotal;
+    document.querySelector('#input-deposit').value = '';
+})
+// Withdraw
+document.querySelector('#withdraw-btn').addEventListener('click', function () {
+    const withdrawInput = document.querySelector('#withdraw-input');
+    const withdrawInputValue = withdrawInput.value;
+    const withdrawInputValueConvert = parseFloat(withdrawInputValue);
+    // Step 02
+    const withdraw = document.querySelector('#withdraw');
+    const withdrawValue = withdraw.innerText;
+    const withdrawValueConvert = parseFloat(withdrawValue);
+    // Step 03
+    withdraw.innerText = withdrawValueConvert + withdrawInputValueConvert;
+    document.querySelector('#withdraw-input').value = '';
+    //   balance
+    const previousTotalBalance=document.querySelector('#balance');
+    const previousTotalBalanceValue=previousTotalBalance.innerText;
+    const previousTotalBalanceValueConvert=parseFloat(previousTotalBalanceValue);
+    console.log(previousTotalBalanceValueConvert)
+    const currentTotalBalance=previousTotalBalanceValueConvert-withdrawInputValueConvert;
+    previousTotalBalance.innerText=currentTotalBalance;
 })
